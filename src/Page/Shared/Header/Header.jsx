f7917200764ddf3001e032a9370e085c954b99ad
ashrafulhossen <b.asrafulhossencht@gmail.com>
2023-06-16 11:19:03 +0600

    Header Section is done

--- src/Page/Shared/Header/Header.jsx
+++ src/Page/Shared/Header/Header.jsx
@@ -11,7 +11,30 @@ import "./Header.css";
 const Header = () => {
 	return (
 		<div className="header">
-			
+			<div className="container">
+				<ul>
+					<li>
+						<FaPhoneAlt />{" "}
+						<span>+001234567890</span>
+					</li>
+					<li>
+						<FaEnvelope />{" "}
+						<span>example@gmail.com</span>
+					</li>
+				</ul>
+				<ul>
+					<li>Theme FAQ</li>
+					<li>Need Helps</li>
+					<li>
+						<FaRegFlag />
+						<span>EN</span>
+					</li>
+					<li>
+						<FaFileInvoiceDollar />
+						<span>USD</span>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
