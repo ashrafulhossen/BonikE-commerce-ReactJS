95985bba3ce72287ae1baf07a71aa86a0c79514e
ashrafulhossen <b.asrafulhossencht@gmail.com>
2023-06-16 11:42:31 +0600

    Navbar section is done

--- src/Page/Shared/Navbar/Navbar.jsx
+++ src/Page/Shared/Navbar/Navbar.jsx
@@ -1,14 +1,86 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import "./Navbar.css"
+import React from "react";
+import { FaChevronDown, FaTable } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-       <div className='navbar'>
-         
-       </div>
-    )
-}
+	return (
+		<div className="navbar">
+			<div className="container">
+				<div>
+					<div className="category">
+						<span>
+							<FaTable /> <span>Category</span>
+						</span>
+						<FaChevronDown />
+					</div>
+				</div>
+				<ul className="menubar">
+					<li>
+						<NavLink
+							to={"/"}
+							className={({ isActive }) =>
+								`navlink ${isActive && "active-navlink"}`
+							}
+						>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to={"/pages"}
+							className={({ isActive }) =>
+								`navlink ${isActive && "active-navlink"}`
+							}
+						>
+							Pages
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to={"/userAccount"}
+							className={({ isActive }) =>
+								`navlink ${isActive && "active-navlink"}`
+							}
+						>
+							User Account
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to={"/vendorAccount"}
+							className={({ isActive }) =>
+								`navlink ${isActive && "active-navlink"}`
+							}
+						>
+							Vendor Account
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to={"/trackOrder"}
+							className={({ isActive }) =>
+								`navlink ${isActive && "active-navlink"}`
+							}
+						>
+							Track My Order
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to={"/contact"}
+							className={({ isActive }) =>
+								`navlink ${isActive && "active-navlink"}`
+							}
+						>
+							Contact
+						</NavLink>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
